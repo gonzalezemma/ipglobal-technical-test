27d14a26246ed6924dd1df6f6f92408a416eed43
gonzalezemma <ggonzalez.emmanuel@gmail.com>
2023-03-09 18:54:02 -0300

    first components and configurations

---
+++ src/components/App/Router/Router.tsx
@@ -0,0 +1,10 @@
+import { lazy, Suspense } from "react";
+import { Route } from "react-router-dom";
+
+const Router = () => (
+  <main>
+    <Suspense></Suspense>
+  </main>
+);
+
+export default Router;
